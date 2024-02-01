7a80013b6bced4fd90391e5ebb74c6039a3c7d08
tony fraser <tony.fraser@gmail.com>
2024-01-31 19:08:47 -0500

    r utils

--- src/load_wgu.py
+++ src/load_wgu.py
@@ -1,7 +1,8 @@
 from daacs.infrastructure.rda_loader import RDALoader
 
 #todo -> fix this loader to be relative. 
-loader = RDALoader("/Users/afraser/Documents/src/daacs-nlp/src/nogit_wgu.rda")
+loader = RDALoader("./nogit/nogit_wgu.rda")
+
 
 # List all the data frame names in the R data file
 data_frame_names = loader.show_all_dataframes()
