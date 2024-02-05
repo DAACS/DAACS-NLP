65f44b31b983be04cfce10f98c6e291e890c9250
tony fraser <afraser@hurricane.lan>
2024-02-04 21:40:21 -0500

    loading data

--- src/app.py
+++ src/app.py
@@ -1,67 +1,65 @@
 import pandas as pd
 from daacs.infrastructure.bootstrap import Bootstrap
 from daacs.infrastructure.wgu_file import WGU_File
-#from daacs.infrastructure.essays import Essays
+from daacs.infrastructure.essays import Essays
 from loguru import logger
 from pyspark.sql.functions import desc, lit, udf, corr, when, lower, col
 
+DAACS_ID="daacs_id"
 b = Bootstrap()
 spark = b.get_spark() 
-
-## These are the ones we are going to try to grade!!
-# essays = spark.read.parquet(b.file_url(WGU_File.essays))
-# 6,000 of these.
-# essays.printSchema() 
-# root
-#  |-- DAACS_ID: integer (nullable = true)
-#  |-- attempt: double (nullable = true)
-#  |-- essay: string (nullable = true)
-
-
 
 ## These are for test and train!! We hae grades for these!! 
 ## Load WGU and filter out the records that are in there twice.
 ratings_columns = ['EssayID', 'TotalScore1', 'TotalScore2', 'TotalScore']
 wgu_ratings_raw = spark.read.option("header", True)\
     .csv(b.file_url(WGU_File.wgu_ratings))\
-    .select(ratings_columns)
-essay_id_counts = wgu_ratings_raw.groupBy("EssayId").count()
-unique_essay_ids = essay_id_counts.filter(col("count") == 1).select("EssayId")
-wgu_ratings = wgu_ratings_raw.join(unique_essay_ids, ["EssayId"])
+    .select(ratings_columns)\
+    .withColumnRenamed("EssayID", DAACS_ID)
+essay_id_counts = wgu_ratings_raw.groupBy(DAACS_ID).count()
+unique_essay_ids = essay_id_counts.filter(col("count") == 1).select(DAACS_ID)
+wgu_ratings = wgu_ratings_raw.join(unique_essay_ids, [DAACS_ID])
 # wgu_ratings.printSchema() 
 # root
-#  |-- EssayID: string (nullable = true)
+#  |-- daacs_id: string (nullable = true)
 #  |-- TotalScore1: string (nullable = true)
 #  |-- TotalScore2: string (nullable = true)
 #  |-- TotalScore: string (nullable = true)
-
+# ->> We probably need more columns.
 
 essays_human_rated = spark.read.parquet(b.file_url(WGU_File.essay_human_ratings))\
-    .distinct() \
-    .join(unique_essay_ids, ["EssayId"])
-
-# essays_human_rated.select("EssayId").distinct().count() = 1094
-# with the join(unique_essay_ids) = 881
-
-# root
-#  |-- EssayID: integer (nullable = true)
-#  |-- essay: string (nullable = true)
-#  |-- file_name: string (nullable = true)
-
-# : 1094 -> distinct human graded essay ids.
-# In [34]: essays_human_rated.printSchema()
-# root
-#  |-- essay: string (nullable = true)
-#  |-- file_name: string (nullable = true)
-#  |-- EssayID: integer (nullable = true)
+    .withColumnRenamed("EssayID", DAACS_ID)\
+    .join(unique_essay_ids, [DAACS_ID])
 
 
-## Read in a few essays
-### Split into Test/Train dataframes
+essays_and_grades = essays_human_rated.join(wgu_ratings, [DAACS_ID])
+# essays_and_grades.show(10)
+# +--------+-------------+--------------------+-----------+-----------+----------+
+# |daacs_id|    file_name|               essay|TotalScore1|TotalScore2|TotalScore|
+# +--------+-------------+--------------------+-----------+-----------+----------+
+# |     148|Essay0148.txt|\tI feel as thoug...|         21|         NA|        NA|
+# |     463|Essay0463.txt|    My SRL result...|         25|         NA|        NA|
+# |     471|Essay0471.txt|My self-regulated...|         21|         NA|        NA|
+# |     496|Essay0496.txt|     My first tho...|         20|         NA|        NA|
+# |     833|Essay0833.txt|In my SRL It stat...|         12|         NA|        NA|
+# +--------+-------------+--------------------+-----------+-----------+----------+
 
 
 
-
-
-## Send them up as "trained essays"
-
+essays_non_graded = spark.read.parquet(b.file_url(WGU_File.essays))\
+    .withColumnRenamed("DAACS_ID", DAACS_ID )
+essays_non_graded.show(10)
+# +--------+-------+--------------------+
+# |daacs_id|attempt|               essay|
+# +--------+-------+--------------------+
+# |   10143|    1.0|After taking the ...|
+# |    2550|    1.0|\nAbout the SRL: ...|
+# |    9496|    1.0|Taking the self-r...|
+# |    5072|    1.0|    Going into th...|
+# |    7224|    1.0|My results on the...|
+# |    3306|    1.0|The self-regulate...|
+# |    3539|    1.0|The DAACS self-re...|
+# |    3174|    1.0|The Self-Regulate...|
+# |    1979|    1.0|My self-regulated...|
+# |    9569|    1.0|The results of my...|
+# +--------+-------+--------------------+
--- src/daacs/infrastructure/essays.py
+++ src/daacs/infrastructure/essays.py
@@ -1,10 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import input_file_name
-from pyspark.sql.functions import regexp_extract, col
+from pyspark.sql.functions import regexp_extract, col, concat_ws, collect_list, first
 from loguru import logger
 import os
 from daacs.infrastructure.bootstrap import Bootstrap
-
+from pyspark.sql import SparkSession, Window 
 
 class Essays:
     """
@@ -19,23 +19,26 @@ class Essays:
         This reads all the essays in from the essays directory, and then writes out a single parquet
         file with EssayID, essay and file_name. 
         """
-        # b = bootsrap ...  
+        # b = Bootstrap() 
         # essays = Essays() 
         # src_dir = f"{b.DATA_DIR}/Essay_Human_Ratings/WGU-Essays/*.txt" 
         # out_dir = f"{b.DATA_DIR}/wgu_trained" 
-        # # essays.to_parquet(src_dir=src_dir, out_dir=out_dir, spark=spark)
-        # b.rename_parquet_files(out_dir, "essay_human_ratings")
+        # essays.to_parquet(src_dir=src_dir, out_dir=out_dir, spark=spark)
+        # b.rename_parquet_files(out_dir, "essay_human_ratings4")
 
-        ## Now go move that essays_human_ratings.parquet over to DATA_DIR/wgu
-        ## and delete teh wgu_trained directory if you want to.
-
-        logger.info(f"Reading essays from: {src_dir}")
-        logger.info(f"Writing parquet file to: {out_dir}")
         essays_df = spark.read.text(src_dir) \
-                    .withColumn("file_name", input_file_name())\
-                    .withColumn("EssayID", regexp_extract(col("file_name"), "Essay(\\d+).txt", 1).cast("int"))\
-                    .withColumnRenamed("value", "essay")\
-                    .withColumn("file_name", regexp_extract("file_name", "([^/]+)$", 1))
-        essays_df.repartition(1).write.mode("overwrite").parquet(out_dir)
+                            .withColumn("file_name", input_file_name())
+
+        # Define a window spec partitioned by file_name
+        windowSpec = Window.partitionBy("file_name")
+
+        # Aggregate the text content per file
+        essays_aggregated = essays_df.withColumn("essay", concat_ws(" ", collect_list("value").over(windowSpec))) \
+                                    .groupBy("file_name").agg(first("essay").alias("essay")) \
+                                    .withColumn("EssayID", regexp_extract(col("file_name"), "Essay(\\d+).txt", 1).cast("int")) \
+                                    .withColumn("file_name", regexp_extract(col("file_name"), "([^/]+)$", 1))
+
+        essays_aggregated.repartition(1).write.mode("overwrite").parquet(out_dir)
+
  
         
