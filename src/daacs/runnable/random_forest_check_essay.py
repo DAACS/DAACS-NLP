58e24222574bd730fdcc17273cc6026aeebae958
tony fraser <afraser@hurricane.lan>
2024-02-15 00:55:06 -0500

    random forest

---
+++ src/daacs/runnable/random_forest_check_essay.py
@@ -0,0 +1,21 @@
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from daacs.infrastructure.sample_data import SampleData
+from daacs.infrastructure.string_utils import StringUtils
+
+# Load the trained model
+pipeline = joblib.load('random_forest_model.joblib')
+
+
+
+bad_student = StringUtils.clean_sentence(SampleData.bad_student)
+new_essay_transformed = pipeline.named_steps['tfidfvectorizer'].transform([bad_student])
+prediction = pipeline.named_steps['randomforestregressor'].predict(new_essay_transformed)
+print(f"Predicted Score for bad student: {prediction[0]}")
+# before cleaning Predicted Score: 19.44 After cleaning  19.51
+
+good_student = StringUtils.clean_sentence(SampleData.good_student)
+new_essay_transformed = pipeline.named_steps['tfidfvectorizer'].transform([good_student])
+prediction = pipeline.named_steps['randomforestregressor'].predict(new_essay_transformed)
+print(f"Predicted Score for good student: {prediction[0]}")
+
