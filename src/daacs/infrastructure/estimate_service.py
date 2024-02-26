1f9e956c64a42329b3703713d030c22d1b1a65fc
tony fraser <afraser@hurricane.lan>
2024-02-26 14:32:03 -0500

    adding the stuff from today

--- src/daacs/infrastructure/estimate_service.py
+++ src/daacs/infrastructure/estimate_service.py
@@ -13,6 +13,8 @@ class Estimates(BaseModel):
     state_of_residence: str = Field(description="estimate what state they are from")
     grade_level: str = Field(description="estimate the grade level or college year level of the person writing this essay")
     gpa: str = Field(description="estimate the current grade point average on a four point scale")
+    awareness: str = Field(description= "estimate the awareness on a scale of one to 10, of the person writing this essay. Awareness phrases include but are not limited to:  should try/ I should, I could/can work on/use, It would be best if I can, I will be consider doing/ , possibly implementing, I need to/ I will need to , I can see the benefit/advantage of , (strategies) may also help to improve/some ways to improve , I have really learned how to,, I would like to work upon and heavily improve.. , It would be very beneficial to,  will help , , I think/ I would commit to , e.g., I think I would be committed to using all of these strategies that I have listed. ")
+
 
 # Load environment variables
 load_dotenv()
