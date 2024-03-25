1b1160b054b42dd9711b5810215f7008387d9bfb
tony fraser <afraser@hurricane.lan>
2024-03-24 22:39:11 -0400

    afraser

--- src/daacs/demos/encode.py
+++ src/daacs/demos/encode.py
@@ -2,6 +2,11 @@ import pandas as pd
 from daacs.infrastructure.bootstrap import Bootstrap
 from daacs.infrastructure.encoders.word_to_vec import WordToVecEncoder
 from daacs.infrastructure.encoders.fast_text import FastTextEncoder
+from daacs.infrastructure.encoders.tfidvectorizer import TfidVectorizerEncoder
+
+
+## this bolts columns onto the end of essays and grades.. 
+## So they're all there, because there's only one essays and grades, eg. 
 
 b = Bootstrap() 
 eg  = b.get_essays_and_grades() 
@@ -9,31 +14,31 @@ eg  = b.get_essays_and_grades()
 print(" *** word2Vec *** ")
 wtv = WordToVecEncoder(bootstrap=b, df=eg) 
 
-eg_plus_wtv_cols: pd.DataFrame  = wtv \
-        .add_tokenized_column()  \
-        .add_vectorized_column() \
-        .get_data() 
-print(eg_plus_wtv_cols.head(3))
-
-
+wtv \
+        .add_tokenized_column(remove_stopwords=True, inbound_text_column='essay', outbound_tokenized_column='tokenized')  \
+        .add_vectorized_column(inbound_tokenized_column='tokenized', outbound_vectorized_column='w2v_vectors')
 print("*** fastex ***")
+
 ftx = FastTextEncoder(bootstrap=b, df=eg) 
-eg_plus_ftx_cols: pd.DataFrame = ftx \
-        .add_tokenized_column()  \
-        .add_vectorized_column() \
+ftx \
+        .add_tokenized_column(remove_stopwords=True, inbound_text_column='essay', outbound_tokenized_column='tokenized')  \
+        .add_vectorized_column(inbound_tokenized_column='tokenized', outbound_vectorized_column='fb_vectors') \
+
+print("*** sklearn tfidvectorizer  ***")
+tfid  = TfidVectorizerEncoder(bootstrap=b, df=eg) 
+
+all_three = tfid \
+        .add_tokenized_column(remove_stopwords=True, inbound_text_column='essay', outbound_tokenized_column='tokenized')  \
+        .add_vectorized_column(inbound_tokenized_column='tokenized', outbound_vectorized_column='scikit_vectors') \
         .get_data() 
-print(eg_plus_ftx_cols.head(3))
 
-# this is also an option...
-# essays_and_grades_plus_wtv_cols: pd.DataFrame  = wtv\
-#         .add_tokenized_column()  \
-#         .add_vectorized_column() \
-# print(wtv.df.head(2))
 
-# To remove stop words, you'd do this.
-# eg_plus_wtv_cols: pd.DataFrame  = wtv \
-#         .add_tokenized_column(remove_stopwords=True)  \
-#         .add_vectorized_column() \
-#         .get_data() 
+
+
+all_three
+
+
+
+
 
 
